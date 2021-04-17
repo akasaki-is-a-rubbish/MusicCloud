76163b9626dc33a91c555db5f16fdeb5448508ec
lideming <14901890+lideming@users.noreply.github.com>
2021-04-17 19:02:13 +0800

    carbox mod

--- src/Settings.ts
+++ src/Settings.ts
@@ -4,7 +4,7 @@ export const settings = {
     // apiBaseUrl: 'http://127.0.0.1:5000/api/',
     debug: true,
     apiDebugDelay: 0,
-    showDownloadOptions: true,
-    showDiscussion: true,
-    showNotes: true,
+    showDownloadOptions: false,
+    showDiscussion: false,
+    showNotes: false,
 };
